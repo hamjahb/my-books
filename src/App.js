033a375ce75a66513672a833b66e62eaa388a91f
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-03 15:44:00 +0300

    xml parsing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+// import {} from 'react-xml-parser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      favorites: [],
+      collection: [],
+
+    }
+  }
+
+  getRequest = () => {
+    const authorId = '1077326';
+    const key = 'AiY0kCwWYFSK3RilpXntbQ';
+
+    const url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/author/list.xml?key=${key}&id=${authorId}&page=`;
+
+    console.log(url);
+
+
+    fetch(url)
+      .then((resp) =>{
+        resp
+        .text()
+        .then( str => {
+          let responseDoc = new DOMParser().parseFromString(str, 'application/xml');
+          console.log(responseDoc);
+        })
+      })
+      
+      .catch((error) => {
+        console.log(error)
+      })
+
+    }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <header>
+          <nav>
+            <collection>Collections filter</collection>
+            <favorites> Favorites filter</favorites>
+          </nav>
+        </header> */}
+
+        {/* <body> */}
+        <h2>body things to be here</h2>
+        {this.getRequest()}
+
+        {/* </body> */}
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
