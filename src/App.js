3053217db43aef29464f96f29ffb5916a8b05857
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-04 09:46:57 +0300

    moved fetch to component did mount and render list

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
     }
   }
 
+  
 
 
-  getRequest = () => {
-    // const authorId = '1077326';
+  componentDidMount() {
     const key = 'AiY0kCwWYFSK3RilpXntbQ';
     const query = 'hobbit'
     const page = '';
@@ -24,44 +24,52 @@ class App extends Component {
 
 
     const url =  `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index.xml?q=${query}&page${page}&key=${key}&search=${search}`
-
-
     // console.log(url);
 
-    
     fetch(url)
-      .then((resp) => {
-        resp.text()
-          .then(str => {
+    .then((resp) => {
+      resp.text()
+        .then(str => {
 
-          // turns response from XML to json
-          let json = parser.xml2js(str, {
-            compact: true,
-            ignoreDoctype: true,
-            attributesKey: "attributes"
-          });      
-          
-          // book search queery location 
-          const querryResult = json.GoodreadsResponse.search.results.work;
-          // console.log(json.GoodreadsResponse.search.results.work)
-          
-          const bookContents = querryResult.map((item, index) => {
-            console.log(item.best_book.title._text)
-            
-          })          
+        // turns response from XML to json
+        let json = parser.xml2js(str, {
+          compact: true,
+          ignoreDoctype: true,
+          attributesKey: "attributes"
+        });      
+        
+        // book search queery location 
+        const querryResult = json.GoodreadsResponse.search.results.work;
+        // console.log(json.GoodreadsResponse.search.results.work)
+        let searcharray= []
+        const bookContents = (querryResult).map((item, index) => {
+          // console.log(item)
+          searcharray.push(item.best_book.title._text)
+        })     
+
+        this.setState({
+          search: searcharray
         })
+        console.log(this.state.search);
       })
+    })
 
-      .catch((error) => {
-        console.log(error)
-      })
-
+    .catch((error) => {
+      console.log(error)
+    })
   }
 
 
 
 
+
+
   render() {
+    const bookRender = this.state.search.map((item, key) =>
+    // console.log(item)
+    <li key={key}>{item}</li>
+    )
+
     return (
       <div className="App">
         {/* <header>
@@ -73,7 +81,9 @@ class App extends Component {
 
         {/* <body> */}
           <h2>body things to be here</h2>
-          {this.getRequest()}
+          <ul>
+            {bookRender}
+          </ul>
 
         {/* </body> */}
       </div>
