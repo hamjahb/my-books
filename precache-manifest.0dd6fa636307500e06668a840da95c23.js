ff4144f6971c29a3216e9a33ae6e2010b7ec23a6
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-06 16:25:45 +0300

    Updates

---
+++ precache-manifest.0dd6fa636307500e06668a840da95c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c7de980ca725a91728c9705ae77aa5",
+    "url": "/my-books/index.html"
+  },
+  {
+    "revision": "10fe82c64d6331efc68a",
+    "url": "/my-books/static/css/main.55a0b458.chunk.css"
+  },
+  {
+    "revision": "e13cce308a9f516c4ab3",
+    "url": "/my-books/static/js/2.4f09e566.chunk.js"
+  },
+  {
+    "revision": "10fe82c64d6331efc68a",
+    "url": "/my-books/static/js/main.2781aa28.chunk.js"
+  },
+  {
+    "revision": "5137c6e2312cf562c7d6",
+    "url": "/my-books/static/js/runtime-main.520d2218.js"
+  }
+]);
